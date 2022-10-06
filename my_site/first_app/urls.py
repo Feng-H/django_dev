dbc7845e61df69ae542812a45c7575d236bd08fb
huangfeng <huangfeng0127@hotmail.com>
2022-10-06 16:04:51 +0800

    urls_reverse 55

--- my_site/first_app/urls.py
+++ my_site/first_app/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
+from django.http.response import HttpResponse
 
+
+def home_view(request):
+    return HttpResponse("first_app Home page")
 # first_app/
 
 urlpatterns = [
-    path('<topic>/',views.news_view),
+    path("", home_view),
+    path("<int:num_page>", views.num_page_view),
+    path('<str:topic>/',views.news_view,name='topic-page'),
 ]
--- my_site/first_app/views.py
+++ my_site/first_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import HttpResponseNotFound
+from django.http import HttpResponseRedirect 
+from django.urls import reverse
+
 # Create your views here.
 
 articles = {
@@ -16,3 +19,10 @@ def news_view(request,topic):
     except:
         result = "no page for that topic!"
         return HttpResponseNotFound(result)
+
+def num_page_view(request,num_page):
+    topics_list = list(articles.keys())
+    topic = topics_list[num_page]
+    
+    return HttpResponseRedirect(reverse('topic-page',args=[topic]))
+
